34f7b5c994380e1052f143cd605fd85ea5e4fa32
Mohamed Amine Laghmouchi <mlaghmouchi@sqli.com>
2025-03-27 09:02:14 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // Development environment
 export const environment = {
   production: true,
-  apiUrl: 'https://e-commerce-project-ochre-seven.vercel.app/api' // Local development API URL
+  apiUrl: 'https://json-server-vercel-omega-lake.vercel.app/api' // Local development API URL
 }; 
